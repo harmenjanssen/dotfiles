b7bb61e17779204ca520562411c4c9fb460a6c44
Harmen Janssen <harmen@grrr.nl>
2016-11-02 11:55:41 +0100

    Tweaked indentation

--- config/fish/functions/gpg-init.fish
+++ config/fish/functions/gpg-init.fish
@@ -4,14 +4,14 @@ function gpg-init
         # is there a process with the pid given in the file?
         [ -f ~/.gnupg/.gpg-agent-info ]
               and kill -0 (cut -d : -f 2 ~/.gnupg/.gpg-agent-info) ^/dev/null
-    end
+        end
          # no, it is not running. Start it!
         killall gpg-agent
         gpg-agent --daemon --no-grab \
             --write-env-file ~/.gnupg/.gpg-agent-info \
             >/dev/null ^&1
     end
-         # get the agent info from the info file, and export it so GPG can see it.
+    # get the agent info from the info file, and export it so GPG can see it.
     set -gx GPG_AGENT_INFO (cut -c 16- ~/.gnupg/.gpg-agent-info)
     set -gx GPG_TTY (tty)
 end
