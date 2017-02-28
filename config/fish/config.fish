595c1f14899886ae8b58f9616acdf11567e081f9
Harmen Janssen <harmen@grrr.nl>
2017-02-28 22:26:09 +0100

    Added sbin to $PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
 #set PATH  ~/.rbenv/shims /usr/local/opt/php55/bin ~/.node/bin /usr/local/bin ./vendor/bin $PATH
-set PATH  /usr/local/opt/php70/bin ~/.node/bin /usr/local/bin ./vendor/bin ./node_modules/.bin $PATH
+set PATH  /usr/local/opt/php70/bin ~/.node/bin /usr/local/bin /usr/local/sbin ./vendor/bin ./node_modules/.bin $PATH
 
 set -x -g APPLICATION_ENV development
 
@@ -16,8 +16,6 @@ set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
 set -x -g EDITOR vim
-
-#source ~/.config/fish/nvm-wrapper/nvm.fish
 
 ssh-add ~/.ssh/id_dsa > /dev/null 2>&1
 ssh-add ~/.ssh/id_rsa > /dev/null 2>&1
