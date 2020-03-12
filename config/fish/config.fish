17355d5aa34616b14f2cbe675fa35ce8c1b01ec2
Harmen Janssen <harmen@grrr.nl>
2020-03-12 09:52:05 +0100

    Default to node10

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -33,4 +33,4 @@ status --is-interactive; and source (rbenv init -|psub)
 
 ensure_tmux_is_running
 
-set -g fish_user_paths "/usr/local/opt/node@8/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths
