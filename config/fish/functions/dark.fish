2ea179ba913d4dd228b7cd97d3a814e39516e4ce
Harmen Janssen <harmen@grrr.nl>
2020-03-12 09:54:14 +0100

    Make dark and light functions switch OS preference

--- config/fish/functions/dark.fish
+++ config/fish/functions/dark.fish
@@ -1,4 +1,6 @@
-# Defined in /var/folders/73/37k7411j1m7g93vqvkbp7swc0000gq/T//fish.ZdZKZ4/dark.fish @ line 2
+# Defined in /var/folders/73/37k7411j1m7g93vqvkbp7swc0000gq/T//fish.NL6w7b/dark.fish @ line 2
 function dark
 	tmux source-file ~/.tmux-themes/dark.conf
+	# Turn on MacOS Dark mode
+	osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = true"
 end
--- config/fish/functions/light.fish
+++ config/fish/functions/light.fish
@@ -1,4 +1,6 @@
-# Defined in /var/folders/73/37k7411j1m7g93vqvkbp7swc0000gq/T//fish.Gpov13/light.fish @ line 2
+# Defined in /var/folders/73/37k7411j1m7g93vqvkbp7swc0000gq/T//fish.enQhbT/light.fish @ line 2
 function light
 	tmux source-file ~/.tmux-themes/light.conf
+	# Turn off MacOS Dark mode
+	osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = false"
 end
