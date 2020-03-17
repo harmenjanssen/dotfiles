9507784da16effaf94b4342c0e4ad207d0b6f19a
Harmen Janssen <harmen@grrr.nl>
2020-03-17 07:12:01 +0100

    Remove commented out cruft

--- config/fish/functions/light.fish
+++ config/fish/functions/light.fish
@@ -1,7 +1,4 @@
-# Defined in /var/folders/73/37k7411j1m7g93vqvkbp7swc0000gq/T//fish.oeTF2V/light.fish @ line 2
+# Defined in /var/folders/73/37k7411j1m7g93vqvkbp7swc0000gq/T//fish.R8qJVK/light.fish @ line 2
 function light
 	sh ~/.config/light.sh
-	#tmux source-file ~/.tmux-themes/light.conf
-	## Turn off MacOS Dark mode
-	#osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = false"
 end
