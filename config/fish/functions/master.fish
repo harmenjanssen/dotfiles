49cbe74a9fce56ce3de8ec2ef0c1c6e307ea9446
Harmen Janssen <harmen@grrr.nl>
2020-10-09 09:14:54 +0200

    Make master function go to default branch
    Regardless of name.
    This is relevant because we're starting to use the name `main` but
    `master` is still muscle-memory.

--- config/fish/functions/master.fish
+++ config/fish/functions/master.fish
@@ -1,3 +1,4 @@
+# Defined in /var/folders/7r/7g32h7k14874x9cwg4mkv8w40000gn/T//fish.7Y5CRk/master.fish @ line 2
 function master
-	git checkout master
+	git checkout (git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
 end
