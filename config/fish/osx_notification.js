089c3ef02f1726865c10f09f1b4ffb2a7e245f3a
Harmen Janssen <harmen@grrr.nl>
2022-04-21 07:23:38 +0200

    Wrap notification logic in try catch block

--- config/fish/osx_notification.js
+++ config/fish/osx_notification.js
@@ -1,17 +1,19 @@
 function run([commandName, status, duration]) {
-  const iterm = Application('iTerm2');
-  // Don't show notification when iTerm is already the frontmost application.
-  if (iterm.frontmost()) {
-    return;
-  }
+  try {
+    const iterm = Application("iTerm2");
+    // Don't show notification when iTerm is already the frontmost application.
+    if (iterm && iterm.frontmost()) {
+      return;
+    }
 
-  const app = Application.currentApplication();
-  app.includeStandardAdditions = true
+    const app = Application.currentApplication();
+    app.includeStandardAdditions = true;
 
-  const messagePrefix = status !== '0' ? 'Failed after' : 'Done in';
-  const message = `${messagePrefix} ${duration} seconds.`;
+    const messagePrefix = status !== "0" ? "Failed after" : "Done in";
+    const message = `${messagePrefix} ${duration} seconds.`;
 
-  app.displayNotification(message, {
+    app.displayNotification(message, {
       withTitle: commandName,
-  });
+    });
+  } catch (err) {}
 }
