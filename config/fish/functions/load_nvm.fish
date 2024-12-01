e8fb50a6518d4ad8b3a5e432b6e5669c17edce14
Harmen Janssen <harmen.janssen@norday.nl>
2024-12-01 07:43:49 +0100

    Add nvm functions to fish

---
+++ config/fish/functions/load_nvm.fish
@@ -0,0 +1,17 @@
+# Defined in /var/folders/7r/7g32h7k14874x9cwg4mkv8w40000gn/T//fish.VzMoLv/load_nvm.fish @ line 1
+function load_nvm --on-variable PWD
+  set -l default_node_version (nvm version default)
+  set -l node_version (nvm version)
+  set -l nvmrc_path (nvm_find_nvmrc)
+  if test -n "$nvmrc_path"
+    set -l nvmrc_node_version (nvm version (cat $nvmrc_path))
+    if test "$nvmrc_node_version" = "N/A"
+      nvm install (cat $nvmrc_path)
+    else if test "$nvmrc_node_version" != "$node_version"
+      nvm use $nvmrc_node_version
+    end
+  else if test "$node_version" != "$default_node_version"
+    echo "Reverting to default Node version"
+    nvm use default
+  end
+end
---
+++ config/fish/functions/nvm_find_nvmrc.fish
@@ -0,0 +1,4 @@
+# Defined in /var/folders/7r/7g32h7k14874x9cwg4mkv8w40000gn/T//fish.SYNAc7/nvm_find_nvmrc.fish @ line 1
+function nvm_find_nvmrc
+    bass source ~/.nvm/nvm.sh --no-use ';' nvm_find_nvmrc
+end
