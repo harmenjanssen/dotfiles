e01f1765e04e5346af387773d5c5e352a3de4853
Harmen Janssen <harmen.janssen@norday.nl>
2024-12-02 14:08:56 +0100

    Fix git prompt for subfolders of repo

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -19,23 +19,24 @@ function fish_prompt
 	set_color normal
 
 	echo -n (prompt_pwd)
-	set -l git_branch (test -d .git && git rev-parse --abbrev-ref HEAD)
-
-	if test $git_branch
-		set -l git_dirty_count (git status --porcelain  | wc -l | sed "s/ //g")
-		echo -n ":"
-		if test $git_dirty_count -gt 0
-			set_color -o red
-		else
-			set_color -o green
-		end
-		echo -n $git_branch
-		if test $git_dirty_count -gt 0
-			echo -n " "✘
-		else
-			echo -n " "✓
-		end
-	end
+    if git rev-parse --is-inside-work-tree >/dev/null 2>&1
+        set -l git_branch (git rev-parse --abbrev-ref HEAD)
+        if test $git_branch
+            set -l git_dirty_count (git status --porcelain  | wc -l | sed "s/ //g")
+            echo -n ":"
+            if test $git_dirty_count -gt 0
+                set_color -o red
+            else
+                set_color -o green
+            end
+            echo -n $git_branch
+            if test $git_dirty_count -gt 0
+                echo -n " "✘
+            else
+                echo -n " "✓
+            end
+        end
+    end
 
     # Finally, the place where actual commands are put in
 	set_color normal
